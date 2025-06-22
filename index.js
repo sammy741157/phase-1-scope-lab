bd430a142a606345ddf9262f2a442a3bd7d4df92
sammy <getembesammy@gmail.com>
2025-06-22 14:24:06 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'john';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'jane'; // This will throw an error because leastFavoriteCustomer is a constant
+}
+
